5115d7c19b48475f7fdeeed0df26b74259d17092
issei.kariya <issei.kariya@zerobillbank.com>
2020-09-07 17:36:05 +0900

    動作確認まで完了

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,34 @@
 mod animation;
 use animation::getter::list;
-use serde::{Deserialize, Serialize};
+
+/* struct MinimalItem {
+  pub id: usize,
+  pub title: String,
+} */
 
 #[async_std::main]
 async fn main() {
-  #[derive(Deserialize, Serialize)]
+  // Reslt型で返却
   let anime = list().await;
-  for item in anime {
-    let data = &item[0];
-    println!("{:?}", data)
+  /* let minimal = anime.iter().map(|item| {
+    let id = item[0].id;
+    let title = &item[0].title;
+    MinimalItem {
+      id: id,
+      title: title.to_string(),
+    };
+  });
+  println!("{:#?}", minimal); */
+  /* println!("{:#?}", anime); */
+
+  match anime {
+    Ok(anime_iter) => {
+      for item in anime_iter {
+        if item.id == 1103 {
+          println!("{:#?}", item)
+        }
+      }
+    }
+    _ => println!("failure"),
   }
 }
